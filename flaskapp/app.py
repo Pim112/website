76012c7cf3c472b2389405cd49399ce5980138bc
P. Bor <pim.bor@hva.nl>
2022-06-10 07:02:21 +0200

    gitignore

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,9 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def testdb():
     return '<h1>hello world.</h1>'
+
 
 if __name__ == '__main__':
     app.run()
--- reactapp/App.tsx
+++ reactapp/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {useEffect, useState} from "react";
 import axios from "axios";
 
